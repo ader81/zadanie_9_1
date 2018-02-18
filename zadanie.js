56ddf09a85530afdd73aa8ac6c176abd6f04f7fa
Adrian Slaby <adrianslaby@gmail.com>
2018-02-18 15:40:42 +0100

    add file

---
+++ zadanie.js
@@ -0,0 +1,12 @@
+function getTriangleArea (a, h) {
+    if (a, h <=0) {
+    console.log('nieprawidłowe dane');
+  }
+  else {
+    return console.log(a*h/2);
+  }
+};
+//console.log(getTriangleArea(10, 6));
+var triangle1Area = getTriangleArea(2, 60);
+var triangle2Area = getTriangleArea(10, 6);  
+var triangle3Area = getTriangleArea(2, 80);
